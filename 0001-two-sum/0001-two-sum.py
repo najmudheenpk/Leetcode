687d0f227850a787a3d8a8a76432fcdd61a06133
Najmudheen PK <107704477+najmudheenpk@users.noreply.github.com>
2026-03-11 19:05:22 +0530

    Delete 0001-two-sum directory

--- 0001-two-sum/0001-two-sum.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        prev_dict = dict()
-
-        for i , num in enumerate(nums):
-            if target - num in prev_dict:
-                return [i , prev_dict[target - num]]
-            prev_dict[num] = i
-        return -1
