289b67c093017d682d6d5bd9b8580c8b1bb9bba1
Najmudheen PK <107704477+najmudheenpk@users.noreply.github.com>
2026-03-17 23:45:48 +0530

    Delete 3731-find-missing-elements directory

--- 3731-find-missing-elements/3731-find-missing-elements.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def findMissingElements(self, nums: List[int]) -> List[int]:
-        output =[]
-        for i in range(min(nums) , max(nums)+ 1):
-            if i not in nums:
-                output.append(i)
-        return output
