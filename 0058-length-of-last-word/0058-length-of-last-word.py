a163fd1e6f56a2c7b283cfcdae5e815b36b8d59f
Najmudheen PK <107704477+najmudheenpk@users.noreply.github.com>
2026-03-25 22:31:35 +0530

    Delete 0058-length-of-last-word directory

--- 0058-length-of-last-word/0058-length-of-last-word.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def lengthOfLastWord(self, s: str) -> int:
-        a =s.split(' ')
-
-        for i in range(len(a)-1,-1,-1):
-            if len(a[i]) != 0:
-                return len(a[i])
